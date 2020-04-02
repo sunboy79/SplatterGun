8e35c833929760ac281bc1e877894ee605c2abb4
sunboy79 <steve.j.hughes@googlemail.com>
2020-04-02 20:48:03 +0000

    Initial particle and basic bunch working.

--- main.jl
+++ main.jl
@@ -1,9 +1,22 @@
-struct Particle
+mutable struct Particle
   px::Real; py::Real; pz::Real
   vx::Real; vy::Real; vz::Real
 end
 
 Particle() = Particle(0,0,0,0,0,0)
 
+mutable struct Bunch
+  particles::Vector{Particle}
+end
+
+Bunch() = Bunch(Particle[])
+
 part = Particle()
-print(part.px)
+
+bunch = Bunch()
+print(bunch)
+
+push!(bunch.particles, part)
+push!(bunch.particles, part)
+push!(bunch.particles, part)
+print(bunch)
