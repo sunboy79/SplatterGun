f52bab858c18390ba400994aa7525bb642c29de1
sunboy79 <steve.j.hughes@googlemail.com>
2020-04-03 08:35:13 +0000

    Simple bunch creation working

--- main.jl
+++ main.jl
@@ -3,20 +3,22 @@ mutable struct Particle
   vx::Real; vy::Real; vz::Real
 end
 
-Particle() = Particle(0,0,0,0,0,0)
+Particle(x::Real) = Particle(x,x,x,x,x,x)
+Particle(vec::Array) = Particle(vec[1],vec[2],vec[3],vec[4],vec[5],vec[6])
+Particle() = Particle(0)
 
-mutable struct Bunch
-  particles::Vector{Particle}
+nparticles = 16
+
+bunch = Array{Particle,1}(undef, nparticles)
+
+using Random
+
+rng = MersenneTwister(1234)
+
+for ii in 1:nparticles
+  #print(randn(rng,6),"\n")
+  bunch[ii] = Particle(randn(rng,6))
+  print(bunch[ii],"\n")
 end
 
-Bunch() = Bunch(Particle[])
-
-part = Particle()
-
-bunch = Bunch()
-print(bunch)
-
-push!(bunch.particles, part)
-push!(bunch.particles, part)
-push!(bunch.particles, part)
-print(bunch)
+#print(bunch)
