85c86807ace4cda3b1dd41d64250926c92d87c4f
sunboy79 <steve.j.hughes@googlemail.com>
2020-04-03 12:14:42 +0000

    Simple push implemented as a function with a timer.

--- main.jl
+++ main.jl
@@ -7,7 +7,7 @@ end
 using Random
 rng = MersenneTwister(1234)
 
-nparticles = 16
+nparticles = 128
 bunch = Array{Particle,1}(undef, nparticles)
 for ii in 1:nparticles
   pos = randn(rng,3)
@@ -15,14 +15,21 @@ for ii in 1:nparticles
   bunch[ii] = Particle(pos, vel)
 end
 
-println("time = 0.0")
-for part in bunch; println(part.pos); end
+function Push(bunch::Array{Particle,1}, dt::Real, nsteps::Integer)
 
-#Advance the bunch
-dt = 1.0e-3
-for part in bunch
-  part.pos = part.pos + part.vel*dt
+  #Advance the bunch
+  for jj = 1:nsteps
+    for part in bunch; 
+      part.pos = part.pos + part.vel*dt; 
+    end
+  end
 end
 
-println("time = ")
+println("time = ", 0.0)
 for part in bunch; println(part.pos); end
+
+@time Push(bunch, 1.0e-3, 100)
+
+println("time = ",0.1 )
+for part in bunch; println(part.pos); end
+
