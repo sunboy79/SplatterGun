eb48618b5f59f4c8b56a8202a290624118a6bc89
sunboy79 <steve.j.hughes@googlemail.com>
2020-04-03 08:48:53 +0000

    First timestep advance to position

--- main.jl
+++ main.jl
@@ -3,19 +3,26 @@ mutable struct Particle
   vel::Array{Real,1}
 end
 
-
-#create a bunch
-nparticles = 16
-
-bunch = Array{Particle,1}(undef, nparticles)
-
+#Create a bunch
 using Random
 rng = MersenneTwister(1234)
 
+nparticles = 16
+bunch = Array{Particle,1}(undef, nparticles)
 for ii in 1:nparticles
   pos = randn(rng,3)
   vel = randn(rng,3)
   bunch[ii] = Particle(pos, vel)
 end
 
-for part in bunch; println(part); end
+println("time = 0.0")
+for part in bunch; println(part.pos); end
+
+#Advance the bunch
+dt = 1.0e-3
+for part in bunch
+  part.pos = part.pos + part.vel*dt
+end
+
+println("time = ")
+for part in bunch; println(part.pos); end
