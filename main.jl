2b1b72c364952ff228f0ed711067190491fc126d
sunboy79 <steve.j.hughes@googlemail.com>
2020-04-03 08:45:30 +0000

    Tidy up generation and print

--- main.jl
+++ main.jl
@@ -1,24 +1,21 @@
 mutable struct Particle
-  px::Real; py::Real; pz::Real
-  vx::Real; vy::Real; vz::Real
+  pos::Array{Real,1}
+  vel::Array{Real,1}
 end
 
-Particle(x::Real) = Particle(x,x,x,x,x,x)
-Particle(vec::Array) = Particle(vec[1],vec[2],vec[3],vec[4],vec[5],vec[6])
-Particle() = Particle(0)
 
+#create a bunch
 nparticles = 16
 
 bunch = Array{Particle,1}(undef, nparticles)
 
 using Random
-
 rng = MersenneTwister(1234)
 
 for ii in 1:nparticles
-  #print(randn(rng,6),"\n")
-  bunch[ii] = Particle(randn(rng,6))
-  print(bunch[ii],"\n")
+  pos = randn(rng,3)
+  vel = randn(rng,3)
+  bunch[ii] = Particle(pos, vel)
 end
 
-#print(bunch)
+for part in bunch; println(part); end
